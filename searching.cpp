ad54004c311f9f9cfcdaed13f34c304e694896c0
Medhavi Sinha <74186903+medhavisinha@users.noreply.github.com>
2021-05-29 15:50:40 +0530

    Add files via upload

---
+++ searching.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <map> 
+using namespace std;
+int main() {
+	map<int, string> Students;
+	Students.insert(std::pair<int, string>(190789, "Akash"));
+	Students.insert(std::pair<int, string>(190790, "Manas"));
+	std::map<int, string>::iterator it = Students.find(190790);
+	if (it != Students.end()) {
+		std::cout << endl << "Key 190790 has the value: => "<< Students.find(190790)->second << '\n';
+	}
+}
