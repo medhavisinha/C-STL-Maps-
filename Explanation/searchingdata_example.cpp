d1324fe34b470fc937fb1dd8e60a04ba66b2e320
Medhavi Sinha <74186903+medhavisinha@users.noreply.github.com>
2021-05-30 01:02:36 +0530

    Update searchingdata_example.cpp

--- Explanation/searchingdata_example.cpp
+++ Explanation/searchingdata_example.cpp
@@ -6,20 +6,20 @@ using namespace std;
 
 int main() {
 
-	////Create a map named 'Students' where the keys will be integers and values wil be strings.
+	//Create a map named 'Students' where the keys will be integers and values wil be strings.
     map<int, string> Students;
     
-    ////Insert values into the map 'Students'. A key of 190789 and a value of Alice has ben inserted into the map. The same gets followed for the next entry.
+    //Insert values into the map 'Students'. A key of 190789 and a value of Alice has ben inserted into the map. The same gets followed for the next entry.
 	Students.insert(std::pair<int, string>(190789, "Akash"));
 	Students.insert(std::pair<int, string>(190790, "Manas"));
     
 	//Using the find() function for the value associated with a key (of 190790).
     std::map<int, string>::iterator it = Students.find(190790);
     
-    ////Use an 'if' statement to check whether the value for the key is found.
+    //Use an 'if' statement to check whether the value for the key is found.
 	if (it != Students.end()) {
     
-		////Print the value of the key alongside with a text on the compiler/editor.
+		//Print the value of the key alongside with a text on the compiler/editor.
         std::cout << endl << "Key 190790 has the value: => "<< Students.find(190790)->second << '\n';
 	}
 }
