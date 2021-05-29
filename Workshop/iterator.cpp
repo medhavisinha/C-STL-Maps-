427edeb7918d23a82f920749748d93b337725db5
Medhavi Sinha <74186903+medhavisinha@users.noreply.github.com>
2021-05-29 19:25:56 +0530

    Update iterator.cpp

--- Workshop/iterator.cpp
+++ Workshop/iterator.cpp
@@ -5,20 +5,20 @@
 using namespace std;
 int main() {
 
-	map<int, string> Students;
-
+map<int, string> Students;
+	
 	Students.insert(std::pair<int, string>(200, "Alice"));
-
+	
 	Students.insert(std::pair<int, string>(201, "John"));
-    
-    Students.insert(std::pair<int, string>(202, "Marcus"));
-
+	
+	Students.insert(std::pair<int, string>(202, "Marcus"));
+	
 	cout << "Map size is: " << Students.size() << endl;
-
-	cout << endl << "Default map Order is: " << endl;
-
+	
+	cout << endl << "Default Map Order is: " << endl;
+	
 	for (map<int, string>::iterator it = Students.begin(); it != Students.end(); ++it) {
-
+		
 		cout << (*it).first << ": " << (*it).second << endl;
 	}
 }
